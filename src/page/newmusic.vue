647b9aae67f1109c019477fe443a0a4521cf4848
zhaochao <zhaochao@rd.netease.com>
2018-04-22 21:58:30 +0800

    update

--- src/page/newmusic.vue
+++ src/page/newmusic.vue
@@ -19,11 +19,6 @@ export default{
   asyncData ({ store }) {
     return store.dispatch('fetchNewSongs');
   },
-  // computed: {
-  //   newSongs (){
-  //     return JSON.parse(this.$store.state.common.newSongs || '{}');
-  //   }
-  // }
 }	
 </script>
 <style scoped>
